a884d3fc09a31d2bf4dc100231c4377ce439282b
John Shin <john.shin53@gmail.com>
2020-10-20 02:10:24 -0400

    updated db URI to work with MongoDB atlas

--- server/config/db.js
+++ server/config/db.js
@@ -1,4 +1,5 @@
 module.exports =
 {
-    "URI": "mongodb://localhost/contacts"
+    //"URI": "mongodb://localhost/contacts"
+    "URI": "mongodb+srv://shinj1232:hyunjune951@mongodbserver.ahflx.azure.mongodb.net/contacts?retryWrites=true&w=majority"
 }
